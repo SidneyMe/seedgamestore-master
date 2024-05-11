10b7bc487bc426d1e244ec92b1b99a6989fc160e
SidneyMe <147240957+SidneyMe@users.noreply.github.com>
2024-05-11 22:45:56 +0300

    changed req

--- diploma2024/wsgi.py
+++ diploma2024/wsgi.py
@@ -8,9 +8,8 @@ https://docs.djangoproject.com/en/5.0.4/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "diploma2024.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'diploma2024.settings')
 
 application = get_wsgi_application()

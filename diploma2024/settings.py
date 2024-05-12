e7af73ede2f88cd5f0448b0ea6e3fc6d96d39c8c
SidneyMe <147240957+SidneyMe@users.noreply.github.com>
2024-05-12 13:12:09 +0300

    heroku db

--- diploma2024/settings.py
+++ diploma2024/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # Change 'default' database configuration with $DATABASE_URL.
 #DATABASES['default'].update(dj_database_url.config(conn_max_age=500, ssl_require=True))
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'), conn_max_age=600)
 }
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()

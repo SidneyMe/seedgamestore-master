8585d2efcde4f8eeb1721a4b3fd5fb7df0d53dda
SidneyMe <147240957+SidneyMe@users.noreply.github.com>
2024-06-30 15:18:46 +0300

    Update EMAIL_BACKEND settings to use environment variables for SMTP configuration

--- diploma2024/settings.py
+++ diploma2024/settings.py
@@ -171,8 +171,8 @@ AUTH_USER_MODEL = 'gamestore.User'
 
 # Email backend settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-mail.outlook.com'
-EMAIL_PORT = 587
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')

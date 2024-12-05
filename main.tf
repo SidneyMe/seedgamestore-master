1b4456c9026943ff25a25e88cc6b9b217aca9a83
SidneyMe <147240957+SidneyMe@users.noreply.github.com>
2024-12-05 20:41:41 +0200

    feat: Update CI/CD pipeline to use Google Cloud credentials and refactor Terraform configuration

--- main.tf
+++ main.tf
@@ -1,53 +1,53 @@
-# Set required provider
 provider "google" {
-  project = "booming-splicer-443411-u6"  # Replace with your GCP project ID
-  region  = "us-central1"      # Choose your desired region
+  project     = "booming-splicer-443411-u6"
+  region      = "us-central1"
+  credentials = var.credentials_json
 }
 
-# Define a small MySQL Cloud SQL instance
+variable "credentials_json" {
+  type        = string
+  description = "The contents of the service account JSON file"
+  sensitive   = true
+}
+
 resource "google_sql_database_instance" "mysql_instance" {
   name             = "mysql-small-db"
   database_version = "MYSQL_8_0"
   region           = "us-central1"
 
   settings {
-    tier = "db-f1-micro" # Smallest instance size
-
+    tier = "db-f1-micro"
     ip_configuration {
       ipv4_enabled = true
     }
   }
 }
 
-# Create a database in the instance
 resource "google_sql_database" "database" {
   name     = "sampledb"
   instance = google_sql_database_instance.mysql_instance.name
 }
 
-# Set a database user
 resource "google_sql_user" "default_user" {
   name     = "admin"
   instance = google_sql_database_instance.mysql_instance.name
-  password = "your_secure_password"  # Use a secure password
+  password = "your_secure_password"
 }
 
-# Define a small Kubernetes cluster
 resource "google_container_cluster" "kubernetes_cluster" {
-  name     = "small-k8s-cluster"
-  location = "us-central1" # Region or zone
-  initial_node_count = 1   # Minimum node count for small cluster
+  name               = "small-k8s-cluster"
+  location           = "us-central1"
+  initial_node_count = 1
 
   node_config {
-    machine_type = "e2-small" # Small machine type
-    disk_size_gb = 10        # Small disk size
+    machine_type = "e2-small"
+    disk_size_gb = 10
     oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform",
+      "https://www.googleapis.com/auth/cloud-platform"
     ]
   }
 }
 
-# Outputs for reference
 output "mysql_instance_connection_name" {
   value = google_sql_database_instance.mysql_instance.connection_name
 }

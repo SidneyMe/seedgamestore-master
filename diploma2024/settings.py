0d7823eaeb7845374cb199a93634c09a1b5f60cf
SidneyMe <147240957+SidneyMe@users.noreply.github.com>
2024-12-05 20:09:04 +0200

    fix: Restore MySQL database settings in settings.py

--- diploma2024/settings.py
+++ diploma2024/settings.py
@@ -84,25 +84,35 @@ TEMPLATES = [
 # WSGI application
 WSGI_APPLICATION = 'diploma2024.wsgi.application'
 
-#sqlite
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
-        }
-    }
-
-# Database settings
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'MySQL80',
-#         'USER': 'root',
-#         'PASSWORD': 'admin',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST', 'localhost'),
+#         'PORT': os.getenv('DB_PORT', '5432'),
 #     }
 # }
+#sqlite
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+#         }
+#     }
+
+# Database settings
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MySQL80',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
 
 # Update DATABASES settings if DATABASE_URL environment variable is present
 # if os.getenv('PRODUCTION') == 'True':
